d80433cb7f566172c37d670713938eae50d97da1
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-10 09:46:22 -0700

    Create post method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,19 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = (longURL, title) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      long_url: longURL,
+      title: title
+    })
+  })
+  .then(response => response.json())
+  .catch(err => console.log(err))
+}
+
