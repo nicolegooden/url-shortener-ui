563258b8e3b2a69260c7c88650dba3a2ebe8d688
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-10 10:33:10 -0700

    Add unit test to track changes of input value based on typing

--- src/components/UrlForm/UrlForm.test.js
+++ src/components/UrlForm/UrlForm.test.js
@@ -11,6 +11,7 @@ describe('Url Form', () => {
   beforeEach(() => {
     mockAddURL = jest.fn();
   })
+
   it('should render correct elements', () => {
     render(
       <UrlForm 
@@ -21,5 +22,18 @@ describe('Url Form', () => {
     expect(screen.getByPlaceholderText('Title...')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('URL to Shorten...')).toBeInTheDocument();
     expect(screen.getByRole('button', {name: 'Shorten Please!'})).toBeInTheDocument();
+  })
+
+  it('should track user interaction with input fields', () => {
+    render(
+      <UrlForm 
+        addURL={mockAddURL}
+      />
+    )
+
+    userEvent.type(screen.getByPlaceholderText('Title...'), 'Harper the Berner');
+    userEvent.type(screen.getByPlaceholderText('URL to Shorten...'), 'longurltoshowallbernerpicturesever.com');
+    expect(screen.getByPlaceholderText('Title...')).toHaveValue('Harper the Berner');
+    expect(screen.getByPlaceholderText('URL to Shorten...')).toHaveValue('longurltoshowallbernerpicturesever.com');
   })
 })
