81c746550f2300e205d3cf151f999eda7648774b
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-10 10:43:25 -0700

    Remove unnecessary mocked data from test files

--- src/components/UrlContainer/UrlContainer.test.js
+++ src/components/UrlContainer/UrlContainer.test.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UrlContainer from './UrlContainer';
-import { getUrls } from '../../apiCalls.js';
-jest.mock('../../apiCalls.js');
 
 describe('Url Container', () => {
-  let mockUrls;
-  beforeEach(() => {
-    getUrls.mockResolvedValue([
-      {
-        id: 1,
-        long_url: 'http://superduperlongurlpleaseshortenit.org',
-        short_url: 'http://localhost:3001/useshorturl/1',
-        title: 'Hey there!'
-      },
-      {
-        id: 2,
-        long_url: 'http://pleaseshortenthislongonemorethanthelastone.org',
-        short_url: 'http://localhost:3001/useshorturl/2',
-        title: 'So, we meet again'
-      }
-    ]);
-
-    mockUrls = [
+    it('should render headings and anchor tags as expected', () => {
+       const mockUrls = [
         {
             id: 1,
             long_url: 'http://superduperlongurlpleaseshortenit.org',
@@ -36,17 +18,15 @@ describe('Url Container', () => {
             short_url: 'http://localhost:3001/useshorturl/2',
             title: 'So, we meet again'
           }  
-    ]
-  })
+       ]
 
-    it('should render headings and anchor tags as expected', async () => {
        render(
         <UrlContainer
           urls={mockUrls}
         />
        ) 
 
-       const firstTitle = await waitFor(() => screen.getByText('Hey there!'))
+       const firstTitle = screen.getByText('Hey there!');
        expect(firstTitle).toBeInTheDocument();
        expect(screen.getByText('http://localhost:3001/useshorturl/1')).toBeInTheDocument();
        expect(screen.getByText('http://superduperlongurlpleaseshortenit.org')).toBeInTheDocument();
