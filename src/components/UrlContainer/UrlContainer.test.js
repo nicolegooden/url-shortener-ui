b99cecad104e32f27977272224b35c0ac27caa29
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-10 12:00:30 -0700

    Add unit test to ensure delete method fires upon clicking delete button

--- src/components/UrlContainer/UrlContainer.test.js
+++ src/components/UrlContainer/UrlContainer.test.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import UrlContainer from './UrlContainer';
 
 describe('Url Container', () => {
-    it('should render headings and anchor tags as expected', () => {
-       const mockUrls = [
+  let mockUrls;
+  let mockRemoveUrl;
+  beforeEach(() => {
+    mockUrls = [
+      {
+          id: 1,
+          long_url: 'http://superduperlongurlpleaseshortenit.org',
+          short_url: 'http://localhost:3001/useshorturl/1',
+          title: 'Hey there!'
+        },
         {
-            id: 1,
-            long_url: 'http://superduperlongurlpleaseshortenit.org',
-            short_url: 'http://localhost:3001/useshorturl/1',
-            title: 'Hey there!'
-          },
-          {
-            id: 2,
-            long_url: 'http://pleaseshortenthislongonemorethanthelastone.org',
-            short_url: 'http://localhost:3001/useshorturl/2',
-            title: 'So, we meet again'
-          }  
-       ]
+          id: 2,
+          long_url: 'http://pleaseshortenthislongonemorethanthelastone.org',
+          short_url: 'http://localhost:3001/useshorturl/2',
+          title: 'So, we meet again'
+        }  
+     ]
 
+    mockRemoveUrl = jest.fn()
+  })
+
+    it('should render headings and anchor tags as expected', () => {
        render(
         <UrlContainer
           urls={mockUrls}
+          removeUrl={mockRemoveUrl}
         />
        ) 
 
@@ -34,4 +42,17 @@ describe('Url Container', () => {
        expect(screen.getByText('http://localhost:3001/useshorturl/2')).toBeInTheDocument();
        expect(screen.getByText('http://pleaseshortenthislongonemorethanthelastone.org')).toBeInTheDocument();
     })
+
+    it('should fire removeUrl method when delete button is clicked', () => {
+      render(
+        <UrlContainer
+          urls={mockUrls}
+          removeUrl={mockRemoveUrl}
+        />
+       ) 
+
+      userEvent.click(screen.getByTestId('2'));
+      expect(mockRemoveUrl).toHaveBeenCalledTimes(1);
+      expect(mockRemoveUrl).toHaveBeenCalledWith(2);
+    }) 
 })
