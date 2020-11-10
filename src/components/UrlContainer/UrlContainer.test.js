df64c33ecf69adc5b30e509c9769f62c944c114c
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-10 10:19:21 -0700

    Add unit test for render of all urls

--- src/components/UrlContainer/UrlContainer.test.js
+++ src/components/UrlContainer/UrlContainer.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import UrlContainer from './UrlContainer';
@@ -7,6 +7,7 @@ import { getUrls } from '../../apiCalls.js';
 jest.mock('../../apiCalls.js');
 
 describe('Url Container', () => {
+  let mockUrls;
   beforeEach(() => {
     getUrls.mockResolvedValue([
       {
@@ -21,8 +22,37 @@ describe('Url Container', () => {
         short_url: 'http://localhost:3001/useshorturl/2',
         title: 'So, we meet again'
       }
-    ])
+    ]);
 
-    it('')
+    mockUrls = [
+        {
+            id: 1,
+            long_url: 'http://superduperlongurlpleaseshortenit.org',
+            short_url: 'http://localhost:3001/useshorturl/1',
+            title: 'Hey there!'
+          },
+          {
+            id: 2,
+            long_url: 'http://pleaseshortenthislongonemorethanthelastone.org',
+            short_url: 'http://localhost:3001/useshorturl/2',
+            title: 'So, we meet again'
+          }  
+    ]
   })
+
+    it('should render headings and anchor tags as expected', async () => {
+       render(
+        <UrlContainer
+          urls={mockUrls}
+        />
+       ) 
+
+       const firstTitle = await waitFor(() => screen.getByText('Hey there!'))
+       expect(firstTitle).toBeInTheDocument();
+       expect(screen.getByText('http://localhost:3001/useshorturl/1')).toBeInTheDocument();
+       expect(screen.getByText('http://superduperlongurlpleaseshortenit.org')).toBeInTheDocument();
+       expect(screen.getByText('So, we meet again')).toBeInTheDocument();
+       expect(screen.getByText('http://localhost:3001/useshorturl/2')).toBeInTheDocument();
+       expect(screen.getByText('http://pleaseshortenthislongonemorethanthelastone.org')).toBeInTheDocument();
+    })
 })
