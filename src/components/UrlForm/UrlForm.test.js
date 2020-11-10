7d44a0d4d2a0ffd4cce62df61effe48304631f23
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-10 10:38:43 -0700

    Add unit test to ensure addURL has been called when user submits form

--- src/components/UrlForm/UrlForm.test.js
+++ src/components/UrlForm/UrlForm.test.js
@@ -3,8 +3,6 @@ import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import UrlForm from './UrlForm';
-import { postUrl } from '../../apiCalls.js';
-jest.mock('../../apiCalls.js');
 
 describe('Url Form', () => {
   let mockAddURL;
@@ -35,5 +33,21 @@ describe('Url Form', () => {
     userEvent.type(screen.getByPlaceholderText('URL to Shorten...'), 'longurltoshowallbernerpicturesever.com');
     expect(screen.getByPlaceholderText('Title...')).toHaveValue('Harper the Berner');
     expect(screen.getByPlaceholderText('URL to Shorten...')).toHaveValue('longurltoshowallbernerpicturesever.com');
+  })
+
+  it('should call addURL when form is submitted', () => {
+    render(
+      <UrlForm 
+        addURL={mockAddURL}
+      />
+    )  
+
+    userEvent.type(screen.getByPlaceholderText('Title...'), 'stack overflow');
+    userEvent.type(screen.getByPlaceholderText('URL to Shorten...'), 'https://stackoverflow.com/questions/tagged/javascript');
+    expect(screen.getByPlaceholderText('Title...')).toHaveValue('stack overflow');
+    expect(screen.getByPlaceholderText('URL to Shorten...')).toHaveValue('https://stackoverflow.com/questions/tagged/javascript');
+    userEvent.click(screen.getByRole('button', {name: 'Shorten Please!'}));
+    expect(mockAddURL).toHaveBeenCalledTimes(1);
+    expect(mockAddURL).toHaveBeenCalledWith('https://stackoverflow.com/questions/tagged/javascript', 'stack overflow');
   })
 })
